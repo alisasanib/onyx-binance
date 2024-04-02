f44686e4eb0b5df5bfe4f2dd7b0ff6d2b275f481
Ali Sasani Babak <ababak@nuclera.com>
2024-04-02 18:37:27 +0100

    add export multiple files

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Chart } from "./Chart";
+export { default as CoinPairSearch } from "./CoinPairSearch";
+export { default as Header } from "./Header";
+export { default as Trades } from "./Trades";
