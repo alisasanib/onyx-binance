47fd111f2c7ad94492858664f2f38e6654012253
Ali Sasani Babak <ababak@nuclera.com>
2024-04-02 16:55:34 +0100

    test: add tests for chart component

---
+++ src/components/Chart/Chart.test.tsx
@@ -0,0 +1,45 @@
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
+import Chart from "./";
+import * as X from "../../websocket";
+
+jest.mock("../../websocket", () => ({
+  binanceStream: {
+    send: jest.fn(),
+    onMessage: jest.fn(),
+    onOpen: jest.fn(),
+    connected: true,
+  },
+}));
+
+describe("<Chart />", () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(() => {
+      return Promise.resolve({
+        json: () => Promise.resolve({ data: {} }),
+      });
+    }) as jest.Mock;
+  });
+  test("should rerender component, unsubscribe previous pair and subscribe to new pair successfully", async () => {
+    const { rerender } = render(<Chart symbol={"BTCUSDT"} />);
+
+    expect(X.binanceStream.send).toHaveBeenCalledWith({ id: 1, method: "SUBSCRIBE", params: ["btcusdt@kline_1h"] });
+    expect(global.fetch).toHaveBeenCalledWith("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d");
+
+    rerender(<Chart symbol={"BNBBTC"} />);
+
+    expect(global.fetch).toHaveBeenCalledWith("https://api.binance.com/api/v3/klines?symbol=BNBBTC&interval=1d");
+    expect(X.binanceStream.send).toHaveBeenCalledWith({ id: 1, method: "UNSUBSCRIBE", params: ["btcusdt@kline_1h"] });
+    expect(X.binanceStream.send).toHaveBeenCalledWith({ id: 1, method: "SUBSCRIBE", params: ["bnbbtc@kline_1h"] });
+  });
+  test("should render chart element properly", () => {
+    render(<Chart symbol={"BTCUSDT"} />);
+
+    expect(X.binanceStream.send).toHaveBeenCalledWith({ id: 1, method: "SUBSCRIBE", params: ["btcusdt@kline_1h"] });
+    expect(global.fetch).toHaveBeenCalledWith("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d");
+
+    const chart = screen.queryByTestId("tradingview-chart");
+
+    expect(chart).not.toBeNull();
+  });
+});
